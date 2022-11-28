19cd1dba5a5a909bf1604cfdfb576262bc788968
Liudmyla <lyudmula1989@ukr.net>
2022-11-28 14:15:09 +0200

    Add js and css

--- index.js
+++ index.js
@@ -53,6 +53,9 @@ function displayCity(e) {
         const temp = Math.round(response.data.daily[0].temperature.day);
         const icon = response.data.daily[0].condition.icon_url;
         const weatherDescription = response.data.daily[0].condition.description;
+        const humidity = response.data.daily[0].temperature.humidity;
+        const wind_speed = response.data.daily[0].wind.speed;
+
 
         const city = document.getElementsByClassName("entered_city")[0];
         city.innerHTML = typedCity;
@@ -69,16 +72,45 @@ function displayCity(e) {
             "current_temperature"
         )[0];
 
+        const currentHumidity = document.getElementsByClassName('humidity')[0];
+        currentHumidity.innerHTML = humidity;
+        const currentWind_speed = document.getElementsByClassName('wind_speed')[0];
+        currentWind_speed.innerHTML = wind_speed;
+        const currentWeatherDescription = document.getElementsByClassName('description')[0];
+        currentWeatherDescription.innerHTML = weatherDescription;
+
         currentTemperature.innerText = temp;
 
+        const weekForecast = document.getElementsByClassName('week_forecast_day');
+        for (let i = 0; i < weekForecast.length; i++) {
+            const nextDateTemp = Math.round(response.data.daily[i + 1].temperature.day);
+            const iconNext = response.data.daily[i + 1].condition.icon_url;
+            const weatherDescription = response.data.daily[i + 1].condition.description;
 
+
+            const next_date = weekForecast[i].getElementsByClassName('next_date');
+            for (let j = 0; j < next_date.length; j++) {
+                next_date[j].innerHTML = nextDateTemp;
+
+            }
+            const weatherIconNext = weekForecast[i].getElementsByClassName('next_img');
+            for (let y = 0; y < weatherIconNext.length; y++) {
+                weatherIconNext[y].setAttribute(
+                    "src",
+                    iconNext
+                );
+                weatherIconNext[y].setAttribute("alt", weatherDescription);
+
+            }
+
+        }
 
     }
 
 
-
     axios.get(`${url}&&key=${apiKey}`).then(showWeather);
 }
+
 
 const inputForm = document.getElementsByClassName("city_button")[0];
 inputForm.addEventListener("click", displayCity);
